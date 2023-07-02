15de6f728f26e7f27f996550ed973147373e4227
Hamza-Mujahid <hamza.mujahid.hussain@gmail.com>
2023-07-02 15:46:30 +0500

    working on my assignment

---
+++ hamza_129800.dart
@@ -0,0 +1,87 @@
+// Name: Muhammad Hamza
+// Roll No: 129800
+import 'dart:io';
+
+void main() {
+  // // Question 1
+  // List<num> listOfNUmbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // List<num> evenNumbers = [];
+  // List<num> oddNumbers = [];
+  // for (num n in listOfNUmbers) {
+  //   if (n % 2 == 0) {
+  //     evenNumbers.add(n);
+  //   } else {
+  //     oddNumbers.add(n);
+  //   }
+  // }
+  // print("Question no 1 👇\n");
+  // print("List of even number $evenNumbers");
+  // print("List of odd numbers $oddNumbers");
+
+  // // Question 2
+  // print("\n Enter a number for fib_Sequence: ");
+  // num someNumber = num.parse(stdin.readLineSync()!);
+  // List<num> fibSequence = [0, 1];
+  // for (int i = 2; i < someNumber; i++) {
+  //   num nextNumber = fibSequence[i - 2] + fibSequence[i - 1];
+  //   fibSequence.add(nextNumber);
+  // }
+  // print("\n Question no 2 👇\n");
+  // print(fibSequence);
+
+  // // Question 3
+  //  print("\n Question no 3 👇\n");
+  // print("\n Enter a number to check if its a prime number or not: ");
+  // num checkNumber = num.parse(stdin.readLineSync()!);
+
+  // bool isPrime = true;
+  // if (checkNumber % 2 == 0) {
+  //   isPrime = false;
+  // }
+
+  // for (num i = 3; i < checkNumber; i += 2) {
+  //   if (checkNumber % i == 0) {
+  //     isPrime = false;
+  //   }
+  // }
+  // if (isPrime) {
+  //   print("$checkNumber is a prime Number");
+  // } else {
+  //   print("$checkNumber is not a prime Number");
+  // }
+
+
+  // // Question 4
+  // print("\n Question no 4 👇\n");
+  // print("\n Enter a number to find the:");
+  // num findFactorial = num.parse(stdin.readLineSync()!);;
+  // num factorial = 1;
+  // // using for loop
+  // // for (num i = 1; i <= findFactorial; i++) {
+  // //   factorial *= i;
+  // // }
+  // // using while loop
+
+  // num i = 1;
+  // while (findFactorial > i) {
+  //   i++;
+  //   factorial *= i;
+  // }
+  // print(factorial);
+
+
+  // // Question 5
+  // print("\n Question no 5 👇\n");
+  // print("\n Enter somer number to find there total like 12345 = 15 :");
+  // String givenNumber = stdin.readLineSync()!;
+  // num answer = 0;
+  // List<String> numbersList = givenNumber.split('');
+  // numbersList.forEach((n) => {
+  //   answer += int.parse(n)
+  // });
+  // print(answer);
+
+
+
+
+}
